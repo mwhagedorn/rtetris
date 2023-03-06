ddb0c05bad13b516407c55f1d3a45c79fa9d7d92
Mike Hagedorn <mike@silverchairsolutions.com>
2023-03-06 10:55:49 -0500

    adds factory methods for each piece

--- lib/rtetris/tetromino.rb
+++ lib/rtetris/tetromino.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+##
+#  This class represents a Tetris shape, and the shape is determined by the passed array of integers
+
 class Tetromino
 
   attr_accessor :current_x
@@ -19,7 +22,57 @@ class Tetromino
     @current_x = 0
     @current_y = 0
   end
+  # The Square or Q shape
+  # 1 1
+  # 1 1
+  # @return Tetromino
+  def self.Q
+    data = [[1,1], [1,1]]
+    tag = 'Q'
+    Tetromino.new(data, tag)
+  end
 
+  # The T shape
+  # 0 1 0
+  # 1 1 1
+  # @return Tetromino
+  def self.T
+    data = [[1,1,1], [0,1,0]]
+    tag = 'T'
+    Tetromino.new(data, tag)
+  end
+  # The I shape
+  # 1
+  # 1
+  # 1
+  # 1
+  # @return Tetromino
+  def self.I
+    data = [[0],[1],[2],[3]]
+    tag = 'I'
+    Tetromino.new(data, tag)
+  end
+
+  # The L shape
+  # 0 1
+  # 0 1
+  # 1 1
+  # @return Tetromino
+  def self.L
+    data = [[1,1],[0,1],[0,1]]
+    tag = 'L'
+    Tetromino.new(data, tag)
+  end
+
+  # The Dog shape or D (LH)
+  # 1 1 0
+  # 0 1 1
+  # @return Tetromino
+  def self.D
+    data = [[0,1,1],[1,1,0]]
+    tag = 'D'
+    Tetromino.new(data, tag)
+  end
 
 
 
