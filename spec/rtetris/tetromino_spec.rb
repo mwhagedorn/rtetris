fd40869176a9dc5c099f2f5e2226d6b8f0f48637
Mike Hagedorn <mike@silverchairsolutions.com>
2023-03-06 10:01:36 -0500

    Adds tests for Q

--- spec/rtetris/tetromino_spec.rb
+++ spec/rtetris/tetromino_spec.rb
@@ -5,20 +5,38 @@ require 'rtetris/tetromino'
 
 RSpec.describe Tetromino do
   context 'when the body data is T' do
-    before do
-      @data = [[1,1,1], [0,1,0]]
-      @tag = 'T'
-      @subject = Tetromino.new(@data, @tag)
-    end
-    it 'has a width of 3' do
-      expect(@subject.width).to eq(3)
-    end
-    it 'has a height of 2' do
-      expect(@subject.height).to eq(2)
-    end
-    it 'has a tag of "T"' do
-      expect(@subject.tag).to eq('T')
+    subject do
+      data = [[1,1,1], [0,1,0]]
+      tag = 'T'
+      Tetromino.new(data, tag)
     end
 
+    it 'has a width of 3' do
+      expect(subject.width).to eq(3)
+    end
+    it 'has a height of 2' do
+      expect(subject.height).to eq(2)
+    end
+    it 'has a tag of "T"' do
+      expect(subject.tag).to eq('T')
+    end
+  end
+
+  context 'when the body data is Q' do
+    subject do
+      data = [[1,1], [1,1]]
+      tag = 'Q'
+      Tetromino.new(data, tag)
+    end
+
+    it 'has a width of 2' do
+      expect(subject.width).to eq(2)
+    end
+    it 'has a height of 2' do
+      expect(subject.height).to eq(2)
+    end
+    it 'has a tag of "Q"' do
+      expect(subject.tag).to eq('Q')
+    end
   end
 end
